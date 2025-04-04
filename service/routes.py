23f15627eb44d3b5ac22da5ed7f5a0b5ed6df1ff
ashes1312 <47263432+ashes1312@users.noreply.github.com>
2025-04-04 09:53:43 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -96,7 +96,6 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-    
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -129,7 +128,6 @@ def update_accounts(account_id):
         account = Account.find(account_id)
         if account:
             account.delete()
-
         return "", status.HTTP_204_NO_CONTENT
         
 ######################################################################
