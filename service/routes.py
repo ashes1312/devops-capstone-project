cb1cd9bf55db6dc7824af9535ca7ddcd83c97302
ashes1312 <47263432+ashes1312@users.noreply.github.com>
2025-04-04 09:47:08 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -80,17 +80,17 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-    @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
-        app.logger.info("Request to read an Account with id: %s", account_id)
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        return account.serialize(), status.HTTP_200_OK
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -114,9 +114,6 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -134,6 +131,7 @@ def update_accounts(account_id):
             account.delete()
 
         return "", status.HTTP_204_NO_CONTENT
+        
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
